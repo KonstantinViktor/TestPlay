c11eed3516734412732ec4b6900b22205945e8fc
kirill123 <kirillgor12506@gmail.com>
2022-10-29 21:07:52 +0300

    MoveCube

--- Assets/Scripts/CubeMove.cs
+++ Assets/Scripts/CubeMove.cs
@@ -2,5 +2,22 @@ using UnityEngine;
 
 public class CubeMove : MonoBehaviour
 {
+    [SerializeField] private float _speed;
+    [SerializeField] private float _radius = 0.2f;
 
+    private Transform cube;
+
+    private void Start()
+    {
+        cube = GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, -transform.up, out hit, _radius))
+            cube.Translate(hit.transform.right * _speed * Time.deltaTime);
+        else
+            cube.Translate(-cube.up * _speed * Time.deltaTime);
+    }
 }
